790a0e20f494254dcd5554622dd71dc84563024e
fhidalgoq <francisco.hidalgo16@gmail.com>
2025-09-02 20:16:00 -0400

    pokemon

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,52 +1,54 @@
-<script>
-export default {
-  data() {
-    return {
-      personas: [
-        { nombre: 'Ana', apellido: 'Perez', fechaNacimiento: '07/01/1964', edad: 61 },
-        { nombre: 'Luis', apellido: 'Gonzales', fechaNacimiento: '20/04/1987', edad: 38 },
-        { nombre: 'María', apellido: 'López', fechaNacimiento: '13/12/1951', edad: 73 },
-      ]
-    };
-  }
-}
+<script setup>
+import { ref } from 'vue';
+const personajes = ref([ 
+            { 
+                "id": "1", 
+                "nombre": "pikachu", 
+                "descripcion": "lorem ipsum uhiuhiuhihoi" 
+            }, 
+            { 
+                "id": "2", 
+                "nombre": "kakuna", 
+                "descripcion": "lorem ipsum uhiuhiuhihoi" 
+            }, 
+            { 
+                "id": "3", 
+                "nombre": "beedrill", 
+                "descripcion": "lorem ipsum uhiuhiuhihoi" 
+            }, 
+            { 
+                "id": "4", 
+                "nombre": "metapod", 
+                "descripcion": "lorem ipsum uhiuhiuhihoi" 
+            }, 
+            { 
+                "id": "5", 
+                "nombre": "rattata", 
+                "descripcion": "lorem ipsum uhiuhiuhihoi" 
+            },
+        ])
 </script>
 
 <template>
 <div class="container mt-4 p-4 border rounded shadow text-center">
-    <h1 class="mb-3 text-center">Tabla de Personas</h1>
+    <h1 class="mb-3 text-center">Tabla de Pokemon En Array</h1>
     <table class="table table-bordered table-striped">
       <thead class="table-dark">
         <tr>
+          <th>ID</th>
           <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Fecha de Nacimiento</th>
-          <th>Edad</th>
+          <th>Descripcion</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{{ personas[0].nombre }}</td>
-          <td>{{ personas[0].apellido }}</td>
-          <td>{{ personas[0].fechaNacimiento }}</td>
-          <td>{{ personas[0].edad }}</td>
-        </tr>
-        <tr>
-          <td>{{ personas[1].nombre }}</td>
-          <td>{{ personas[1].apellido }}</td>
-          <td>{{ personas[1].fechaNacimiento }}</td>
-          <td>{{ personas[1].edad }}</td>
-        </tr>
-        <tr>
-          <td>{{ personas[2].nombre }}</td>
-          <td>{{ personas[2].apellido }}</td>
-          <td>{{ personas[2].fechaNacimiento }}</td>
-          <td>{{ personas[2].edad }}</td>
-        </tr>
-      </tbody>
+      <tr v-for="personaje in personajes" :key="personaje.id">
+        <td>{{ personaje.id }}</td>
+        <td>{{ personaje.nombre }}</td>
+        <td>{{ personaje.descripcion }}</td>
+      </tr>
+    </tbody>
     </table>
   </div>
 </template>
-
 <style>
 </style>
